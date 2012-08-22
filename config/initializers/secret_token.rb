571ffb84115d5fbfc1db5e4b26145b620c56a1a9
Sang Won Seo <sang@coshx.com>
2012-08-22 17:41:41 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BibleShare::Application.config.secret_token = '8778a6ed5c839dd914ceee904f6b62d6f712a9e1029fa3bb741a5216fc66a4423020d58486bde89d872b30f22994726557a44c7b752693c12a608fee26f42c38'
