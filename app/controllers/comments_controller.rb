0063a1657189ad20d6b2ad116a60fd8e5dd561d3
Sang Won Seo <sang@coshx.com>
2012-08-23 02:50:24 -0400

    enables commenting while reading

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,10 +25,11 @@ class CommentsController < ApplicationController
   # GET /comments/new.json
   def new
     @comment = Comment.new
-
+    @index = params[:index]
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @comment }
+      format.js{}
     end
   end
 
@@ -44,7 +45,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to passage_path(Post.find(params[:comment][:post_id]).passage_id), notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { render action: "new" }
