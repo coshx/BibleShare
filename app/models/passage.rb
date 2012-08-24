a7401686f066a612f233135b8b5c7711c25e3119
Sang Won Seo <sang@coshx.com>
2012-08-23 21:43:57 -0400

    makes subcontents to be destroyed when the parent content is deleted

--- app/models/passage.rb
+++ app/models/passage.rb
@@ -1,5 +1,5 @@
 class Passage < ActiveRecord::Base
-  has_many :posts
+  has_many :posts, :dependent => :destroy
   belongs_to :user
-  attr_accessible :bible, :content, :title, :scripture
+  attr_accessible :bible, :content, :title, :scripture, :user_id
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
-  has_many :comments
+  has_many :comments, :dependent => :destroy
   belongs_to :passage
   belongs_to :user
-  attr_accessible :content, :passage_id
+  attr_accessible :content, :passage_id, :user_id
 end
